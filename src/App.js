27a9b5e8db076ecd93801351b9640f1046eb4e2c
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-09 04:38:46 +0600

    routes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from "react";
-import Header from "./components/Header/Header";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About/About";
+import Inventory from "./components/Inventory/Inventory";
+import Orders from "./components/Orders/Orders";
 import Shop from "./components/Shop/Shop";
+import Main from "./layouts/Main";
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main />,
+      children: [
+        {
+          path: "/",
+          element: <Shop />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/inventory",
+          element: <Inventory />,
+        },
+        {
+          path: "/orders",
+          element: <Orders />,
+        },
+      ],
+    },
+  ]);
   return (
     <div>
-      <Header />
-      <Shop />
+      <RouterProvider router={router} />
     </div>
   );
 };
