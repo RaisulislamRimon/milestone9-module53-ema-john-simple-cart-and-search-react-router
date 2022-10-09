21bfe4c50d1f6cd52fb86ecfeea7a4218e8e65a9
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-09 11:37:27 +0600

    update Orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Orders = () => {
   const { products, initialCart } = useLoaderData(); // { products, initialCart }
+  const [cart, setCart] = useState(initialCart);
   return (
     <div>
       <h1>Orders {products.length}</h1>
-      <h1>initial cart {initialCart.length}</h1>
+      <h1>initial cart {cart.length}</h1>
     </div>
   );
 };
