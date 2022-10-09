0b6785292bd9cf8de43b1786135c073332684a48
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-09 11:42:58 +0600

    Cart error in Orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,20 @@
 import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import Cart from "../Cart/Cart";
 
 const Orders = () => {
   const { products, initialCart } = useLoaderData(); // { products, initialCart }
   const [cart, setCart] = useState(initialCart);
   return (
     <div>
-      <h1>Orders {products.length}</h1>
-      <h1>initial cart {cart.length}</h1>
+      <div className="shop-container">
+        <div className="products-container">
+
+        </div>
+        <div className="cart-container">
+          <Cart/>
+        </div>
+      </div>
     </div>
   );
 };
